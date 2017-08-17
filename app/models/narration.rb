6d0d278ded8f0448552ce98b1e0c5123c4ab2cb6
Nabeel Khan <mnkhan@fullsail.edu>
2017-08-17 16:06:22 -0700

    Added Tester

--- app/models/narration.rb
+++ app/models/narration.rb
@@ -6,4 +6,28 @@ class Narration < ActiveRecord::Base
 		self.update(annotated_arabic: nil)
 	end
 
+	def self.bulk_add_narrator(name)
+
+		n = HadithCollection.first.narrations
+		ahadith = n.where("arabic like ?", "%#{name}%")
+
+		ahadith.each do |h|
+
+			original = h.arabic
+
+			if h.annotated_arabic.present?
+				annot = h.annotated_arabic
+				new_version = annot.gsub!(name, '<span class="narrator id-this">'+name+'</span>')
+				h.update(annotated_arabic: new_version)
+			else
+				h.update(annotated_arabic: original)
+				annot = h.annotated_arabic
+				new_version = annot.gsub!(name, '<span class="narrator id-this">'+name+'</span>')
+				h.update(annotated_arabic: new_version)
+			end
+
+		end
+
+	end
+
 end
